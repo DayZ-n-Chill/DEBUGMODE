40d08c99ef12f965b35d53b411026da7dfe1dbe8
Brian Orr <DrkDevil@users.noreply.github.com>
2023-12-12 19:56:09 -0600

    Cleanup instructions

--- Utils/Setup.bat
+++ Utils/Setup.bat
@@ -1,10 +1,15 @@
 @echo off
 SETLOCAL
-
-REM Set the source Workshop directory
+REM This script will create a junction to the Workshop directory on the P drive
+REM You just need to  to provide the path to the Workshop directory.
 SET "WorkshopDir=C:\Program Files (x86)\Steam\steamapps\common\DayZ\!Workshop"
+REM The secondayt workshop directory for those people on your dev team who just refuse to conform 
+REM and is installing their steam on a seperate drive than where the rest of the team is installing it 😉
+SET "SecondaryWorkshopDir=D:\Program Files (x86)\Steam\steamapps\common\DayZ\!Workshop"
 
-REM Set the target directory on the P drive
+REM Symlink your !Workshop directory to your P drive
+REM This give the .BAT file access to the hidden !workshop folder. 
+REM (Dont modify this folder.)
 SET "MODDIR=P:\Mods"
 
 REM Check if the P drive is mounted
