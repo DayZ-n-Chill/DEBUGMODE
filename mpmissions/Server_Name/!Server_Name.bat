72a495f98f4ea45864d08cb0f1ed3020aa29ad0e
DayZ n' Chill™ <brian@dayznchill.com>
2023-12-14 12:05:45 -0600

    Scratch
    Rebuilt what I did in 2.0 for optimization something broke in it the last version.  Going to fix it all here in 1.5 first

--- mpmissions/Server_Name/!Server_Name.bat
+++ mpmissions/Server_Name/!Server_Name.bat
@@ -1,29 +1,20 @@
 @echo off
-@REM List of client mods used on this server. / NOTE: must be outside the "!workshop" directory - I recommend the DayZ root folder
 SET "MODS="
-
-@REM List of mods used that run only on the server. 
 SET "SEVERSIDEMODS="
-
-@REM Directory to your Servers Config Files
 SET "SERVERCFG=DEBUGMODE\mpmissions\Server_Name\serverDZ.cfg"
-
-@REM Directory to your Servers Mission Files 
 SET "MISSION=DEBUGMODE\mpmissions\Server_Name\dayzOffline.chernarusplus"
-
-@REM Directory to your Servers Profiles Folder
 SET "PROFILES=DEBUGMODE\mpmissions\Server_Name\profiles"
-
-@REM !!! Dont Edit this stuff below this line unless you know what you are doing.!!!
-
-@REM Local IP Address for server to run on.
+SET "CLIENTDIAGLOGS=DEBUGMODE\!ClientDiagLogs"
 SET "LOCALHOST=127.0.0.1:2302"
 
-@REM Path to the DayZDiag_x64.exe from this file 
-cd ../../../
+SET "GAMEDIR=C:\Program Files (x86)\Steam\steamapps\common\DayZ"
+IF NOT EXIST "%GAMEDIR%" (
+    powershell -Command "Write-Host 'Error: DayZ Installation directory not found at %GAMEDIR%' -ForegroundColor Red"
+) ELSE (
+    powershell -Command "Write-Host 'Success: DayZ Installation directory found.' -ForegroundColor Green"
+)
 
-@REM Start Game
-start DayZDiag_x64.exe -mod=%MODS% -profiles=!ClientDiagLogs -connect=%LOCALHOST% -battleye=0 -filepatching=1
+cd %GAMEDIR%
 
-@REM Start The Server 
+start DayZDiag_x64.exe -mod=%MODS% -profiles=%CLIENTDIAGLOGS% -connect=%LOCALHOST% -battleye=0 -filepatching=1
 start DayZDiag_x64.exe -server -noPause -doLogs -mission=%MISSION% -config=%SERVERCFG% -profiles=%PROFILES% -mod=%MODS% -serverMod=%SEVERSIDEMODS% -filepatching=1
